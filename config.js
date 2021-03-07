1a9409c1f43d102cb42c6ca91cb6dfec78537062
Radd <sriramgoparaju99@gmail.com>
2021-03-07 10:43:30 +0530

    added config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	SECRET_KEY_JWT:
+		"a very secret key that must not be seen by others $123Radd99",
+	MONGODB:
+		"mongodb+srv://Radd:LRaG2ja7qzhSaqFs@cluster0.vmjwb.mongodb.net/merng?retryWrites=true&w=majority",
+};
